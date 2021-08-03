1162f41ed236755a233f231201c2abf43341588a
Avisa-SEI <aap571420@gmail.com>
2021-08-03 13:31:59 -0700

    wip:

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -5,7 +5,7 @@ import { Grid } from '@material-ui/core';
 export default function Post({post}) {
 	
     return (
-        <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid container direction="row" justifyContent="center" >
 			<Grid item xs={12} sm={6} md={6} lg={5}>
 				<div className="card large">
 					<div className="card-image">
